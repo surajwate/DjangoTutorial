e1d9f82598b24a6d6ad4320909ed26ebe275bb68
Suraj Wate <suraj.wate@gmail.com>
2024-09-26 16:27:32 +0100

    Refactor detail view to use get_object_or_404

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from .models import Question
-from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 from django.shortcuts import render
 
@@ -12,10 +12,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 
